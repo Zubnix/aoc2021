b1f111f99b07b52789ff58e8751032dc9c6b8d6d
erik <bleh>
2021-12-01 14:16:29 +0100

    first task

---
+++ 1.ts
@@ -0,0 +1,43 @@
+import { input1 } from './1-input.ts'
+
+const measurements = input1.trim().split('\n').map(element => element.trim()).map(element => Number.parseInt(element))
+
+function first () {
+  const countContext = { count: 0, previous: measurements[0] }
+  measurements.reduce((context, current) => {
+    if (context.previous < current) {
+      context.count++
+    }
+    context.previous = current
+    return context
+  }, countContext)
+
+  console.log(JSON.stringify(countContext))
+}
+first()
+
+function second () {
+  const countContext: { sumList: number[][]; count: number } = { count: 0, sumList: [] }
+
+  measurements.reduce((context, current) => {
+    const incompleteSums = countContext.sumList.filter(sum => sum.length < 3)
+    incompleteSums.forEach(incompleteSum => incompleteSum.push(current))
+    countContext.sumList.push([current])
+
+    const completeSums = countContext.sumList.filter(sum => sum.length === 3)
+    console.assert(completeSums.length <= 2)
+
+    if(completeSums.length === 2) {
+      const [firstSum, secondSum] = completeSums
+      if (firstSum.reduce((sum, element) => sum + element) < secondSum.reduce((sum, element) => sum + element)) {
+        context.count++
+      }
+      countContext.sumList = countContext.sumList.filter(sum => sum !== firstSum)
+    }
+
+    return context
+  }, countContext)
+
+  console.log(JSON.stringify(countContext))
+}
+second()
